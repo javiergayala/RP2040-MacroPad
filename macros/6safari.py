3ef530a3d41b918f702ab4ab2c9e3a681b4fd970
Javier Ayala <jayala@rackspace.com>
2021-09-02 13:05:51 -0400

    refactor(safari): remove redundant keystrokes

--- macros/6safari.py
+++ macros/6safari.py
@@ -8,21 +8,17 @@ app = {  # REQUIRED dict, must be named 'app'
     "macros": [  # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (SAFARI_COLOR, "< Back", [SAFARI_KEY, [Keycode.COMMAND, "["]]),
-        (SAFARI_COLOR, "Fwd >", [SAFARI_KEY, [Keycode.COMMAND, "]"]]),
-        (SAFARI_COLOR, "Up", [SAFARI_KEY, [Keycode.SHIFT, " "]]),  # Scroll ]up
+        (SAFARI_COLOR, "< Back", [Keycode.COMMAND, "["]),
+        (SAFARI_COLOR, "Fwd >", [Keycode.COMMAND, "]"]),
+        (SAFARI_COLOR, "Up", [Keycode.SHIFT, " "]),  # Scroll ]up
         # 2nd row ----------
-        (
-            SAFARI_COLOR,
-            "< Tab",
-            [SAFARI_KEY, [Keycode.CONTROL, Keycode.SHIFT, Keycode.TAB]],
-        ),
-        (SAFARI_COLOR, "Tab >", [SAFARI_KEY, [Keycode.CONTROL, Keycode.TAB]]),
+        (SAFARI_COLOR, "< Tab", [Keycode.CONTROL, Keycode.SHIFT, Keycode.TAB]),
+        (SAFARI_COLOR, "Tab >", [Keycode.CONTROL, Keycode.TAB]),
         (SAFARI_COLOR, "Down", " "),  # Scroll down
         # 3rd row ----------
-        (SAFARI_COLOR, "Reload", [SAFARI_KEY, [Keycode.COMMAND, "r"]]),
-        (SAFARI_COLOR, "Home", [SAFARI_KEY, [Keycode.COMMAND, "H"]]),
-        (SAFARI_COLOR, "Private", [SAFARI_KEY, [Keycode.COMMAND, "N"]]),
+        (SAFARI_COLOR, "Reload", [Keycode.COMMAND, "r"]),
+        (SAFARI_COLOR, "Home", [Keycode.COMMAND, "H"]),
+        (SAFARI_COLOR, "Private", [Keycode.COMMAND, "N"]),
         # 4th row ----------
         (0x000000, "<--", []),  # Adafruit in new window
         (SAFARI_COLOR, "Safari", SAFARI_KEY),
